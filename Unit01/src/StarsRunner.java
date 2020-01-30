9e71d9ff97e9ef58874245dfe2b8ad7337954082
sk28832 <33585911+sk28832@users.noreply.github.com>
2020-01-30 13:18:04 -0800

    Unit01 Assignments

---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,43 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes bruh = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwoBlankLines();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printTwentyStars();
+      bruh.printTwentyDashes();
+      bruh.printASmallBox();
+      bruh.printABigBox();
+   }
+}
