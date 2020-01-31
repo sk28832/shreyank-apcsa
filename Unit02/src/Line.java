eeacc98b98459655c73d40470b063e48a2b291a6
sk28832 <33585911+sk28832@users.noreply.github.com>
2020-01-31 12:40:31 -0800

    Unit 2 Assignments

---
+++ Unit02/src/Line.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( float x1, float y1, float x2, float y2 )
+	{
+		return (y2-y1)/(x2-x1);
+	}
+
+}
